817833bd2cabe057577be058fe86d68f752e3f1a
Syam Chand Banisetti <syamchandbanisetti@gmail.com>
2025-04-16 19:07:22 +0530

    Create puzzle8.py

---
+++ utils/puzzle8.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run_puzzle8_app():
+    st.header("🧩 8-Puzzle Solver")
+    st.info("Interactive A* 8-puzzle solver coming soon!")
+    st.image("https://upload.wikimedia.org/wikipedia/commons/8/8c/15-puzzle-01.jpg", width=300)
