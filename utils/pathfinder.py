86c5e66aa702d9890a3325481e65881b051f186d
Syam Chand Banisetti <syamchandbanisetti@gmail.com>
2025-04-16 19:09:04 +0530

    Create pathfinder.py

---
+++ utils/pathfinder.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+
+def run_pathfinder_app():
+    st.header("🧭 Pathfinding Maze Solver")
+    st.info("A* or DFS-based maze solving interface")
+    st.image("https://upload.wikimedia.org/wikipedia/commons/5/5f/A_star_algorithm_animation.gif", width=400)
